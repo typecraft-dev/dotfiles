f1a8263e5ac3a6c7fd6b4d3d5d5f80ca6f4cf23c
Tophie Bear <chris@typecraft.dev>
2024-05-13 11:10:32 -0400

    add rename

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -37,6 +37,8 @@ return {
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
     end,
   },
 }
