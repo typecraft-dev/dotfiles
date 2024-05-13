eb59905ba26548cdf9d45047bb4640340e5a5fdd
Tophie Bear <chris@typecraft.dev>
2024-05-13 11:18:56 -0400

    create symlinks

--- install.sh
+++ install.sh
@@ -1,4 +1,23 @@
-#! bin/sh
+#! /bin/bash
 
 mkdir -p ~/.config/
-ln -s ../nvim ~/.config/nvim
+
+create_symlinks() {
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
+
+    # Get a list of all files in this directory that start with a dot.
+    files=$(find -maxdepth 1 -type f -name ".*")
+
+    # Create a symbolic link to each file in the home directory.
+    for file in $files; do
+        name=$(basename $file)
+        echo "Creating symlink to $name in home directory."
+        rm -rf ~/$name
+        ln -s $script_dir/$name ~/$name
+    done
+
+    ln -s $script_dir/nvim ~/.config/
+}
+
+create_symlinks
